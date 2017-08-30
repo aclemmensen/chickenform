103586a71148d3577f602239beb2e3c62a0cdcc2
Asbjørn Clemmensen <ac@givetwise.dk>
2017-08-31 00:24:46 +0200

    Cleanup and some docs

--- hello.scm
+++ hello.scm
@@ -3,6 +3,7 @@
 (use alist-lib)
 (require-extension typed-records)
 
+; helper for reloading code
 (define l (lambda () (load "/app/hello.scm")))
 
 (defstruct vpc 
@@ -15,36 +16,43 @@
 
 (defstruct sg ingress egress)
 
-(define (ec2->json name ec2)
-	`((aws_instance .
-		((,name . 
-						((ami . ,(ec2-ami-id ec2))
-						 (subnet_id . ,(ec2-subnet-id ec2))
-						 (source_dest_check . #f)
-						 (key . ,(ec2-key ec2))))))))
+; map ec2
+(define (ec2->tf ec2)
+	`((ami . ,(ec2-ami-id ec2))
+		(subnet_id . ,(ec2-subnet-id ec2))
+		(source_dest_check . #f)
+		(key . ,(ec2-key ec2))))
 
-(define prod-vpc (make-vpc id: "vpc-11111" subnets: #("subnet-1111")))
+; mappings of structs to tf types and mappers
+(define (struct->mapper elem)
+	(cond
+		((ec2? elem) `(aws_instance . ,ec2->tf))))
 
-(define (xxx ll)
+; tf boilerplate and struct mapping
+(define (tf name elem)
+	(let* ([sm (struct->mapper elem)]
+				 [type (car sm)]
+				 [f (cdr sm)])
+		`((,type . 
+						 ((,name . ,(f elem)))))))
+
+; merge tf structures by type
+(define (merge ll)
 	(fold (lambda (elem res)
 					(let* ([key (car elem)]
 								 [val (cdr elem)]
 								 [exist (alist-ref/default res key '())])
 						(alist-update key (append val exist) res))) '() ll))
 
-(define (x) (xxx '((a . (h . 2)) (a . (a . 3)) (b . (x . 1)))))
+; merge all given structs into one tf structure
+(define (generate . ll) (merge (foldl append '() ll)))
 
-(define (generate #!rest ll) (xxx (foldl append '() ll)))
+(define prod-vpc (make-vpc id: "vpc-11111" subnets: #("subnet-1111")))
 
-(define (woop) 
-	(generate
-		(ec2->json 'master (make-ec2 type: "t2.medium" ami-id: "ami-111"))
-		(ec2->json 'public (make-ec2 type: "t2.medium" ami-id: "ami-111"))
-		(ec2->json 'private (make-ec2 type: "t2.large" ami-id: "ami-222"))))
+(define (warp)
+	(generate 
+		(tf 'moop (make-ec2 type: "t2" ami-id: "wat"))  
+		(tf 'foop (make-ec2 type: "t2" ami-id: "wat"))))
 
-(define (jwoop) (print (json->string (woop))))
+(define (jwarp) (print (json->string (warp))))
 
-(define (cluster key)
-	(let* ((i (make-ec2 type: "t2.medium" ami-id: "ami-1111"))
-				 (m (master i key)))
-		(json->string (ec2->json 'master i))))
